e0222593754ec70517620d9306f10c01d0ec779a
Renanprogramador11 <renan.jamaicass1@gmail.com>
2025-02-28 23:47:22 +0000

    supertrunfo1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
+// Desafio Super Trunfo - Países          
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+       
+    int  turistico , codigo , area ;
+    float pib , populacao ;
+    char estado [50] , cidade [50] , carta [50] ;
 
-    return 0;
+    printf("Digite sua carta 1 : \n");
+    scanf("%s", &carta);
+
+    printf("Digite o seu estado : \n");
+    scanf("%s", &estado);
+
+    printf("Digite a sua cidade : \n" );
+    scanf("%s", cidade);
+
+    printf("Digite a população : \n" );
+    scanf ("%d" , populacao );
+
+    printf("Digite o PIB : \n" );
+    scanf("%f", pib );
+
+    printf("Digite os pontos turísticos : \n" );
+    scanf("%d", turistico );
+
+    printf ("  carta  : %s \n estado : %s \n ", carta , estado );
+    printf ("codigo : %s \n " , estado );
+    printf ("nome da cidade : %s \n " , cidade );
+    printf ("População : %d \n " , populacao );
+    printf ("Área   : %f  \n " , area );
+    printf ("PIB    : %f \n " , pib );
+    printf ("Números de Pontos Túristicos : %d " , turistico );
+
+   
+
+return 0;
+
 }
